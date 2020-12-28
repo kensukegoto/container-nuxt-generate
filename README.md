eeefe038495d23fbdbc87b3cbba6022f9c2205de
kensuke goto <kensuke.goto.1981@gmail.com>
2020-12-28 16:22:42 +0900

    追加：ExpressでLinuxコマンド試す”touch”する

---
+++ README.md
@@ -0,0 +1,9 @@
+# コミット
+
+```
+追加：Expressを動かす
+追加：ExpressでLinuxコマンド試す”touch”する
+追加：Volumeをくっつけてtouchする
+追加：Volumeでnuxt generateする
+追加：Volumeでbuildしてgenerateする
+```
---
+++ image/app/index.js
@@ -0,0 +1,25 @@
+const {exec} = require('child_process');
+const express = require('express');
+const { stderr } = require('process');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+
+  exec('touch kensuke.txt',(err,stdout,stderr) => {
+    if(err){
+      res.send(JSON.stringify(err));
+      return;
+    }
+    res.send('Hello World !!');
+  });
+
+  
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
