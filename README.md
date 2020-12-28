1334d05414539895eec130180f4d79ba0cd6be4a
kensuke goto <kensuke.goto.1981@gmail.com>
2020-12-28 16:37:11 +0900

    追加：Volumeをくっつけてtouchする

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 追加：Volumeでnuxt generateする
 追加：Volumeでbuildしてgenerateする
 ```
+
+# Dockerコマンド
+
+```
+# Volumeくっつける
+docker run -d --name express-1 -p 80:8080 --rm -v /Users/gotoukensuke/Desktop/ctr-nuxt-generate/volume:/volume my-express:1.5
+```
--- image/app/index.js
+++ image/app/index.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
 
-  exec('touch kensuke.txt',(err,stdout,stderr) => {
+  exec('cd /volume && touch kensuke.txt',(err,stdout,stderr) => {
     if(err){
       res.send(JSON.stringify(err));
       return;
