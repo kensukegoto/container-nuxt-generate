3bff0c539fadfe75217a53e4d21f8cb39a7c238e
kensuke goto <kensuke.goto.1981@gmail.com>
2021-01-31 10:34:41 +0900

    変更：nodeコンテナではなく、AWS CLIコマンドをインストール・実行できるイメージを作成する

--- image/ctr-nuxt-gen-2/nuxt.config.js
+++ image/ctr-nuxt-gen-2/nuxt.config.js
@@ -37,8 +37,8 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
-  buildDir: '/volume/.nuxt',
-  generate: {
-    dir: '/volume/dist'
-  }
+  // buildDir: '/volume/.nuxt',
+  // generate: {
+  //   dir: '/volume/dist'
+  // }
 }
